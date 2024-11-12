9d641f9f5e24b4b37384b8f9e53cdd4c639b8619
이태한 <121335699+TaehanLee07@users.noreply.github.com>
2024-11-12 17:27:01 +0900

    Add files via upload

--- Spring 시작/firstProject/src/main/java/com/example/firstProject/controller/ArticleController.java
+++ Spring 시작/firstProject/src/main/java/com/example/firstProject/controller/ArticleController.java
@@ -3,12 +3,15 @@ package com.example.firstProject.controller;
 import com.example.firstProject.dto.ArticleForm;
 import com.example.firstProject.entity.Article;
 import com.example.firstProject.repository.ArticleRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+
 @Controller
+@Slf4j
 public class ArticleController {
     // DI 의존주입 (인스턴스화 할 필요없이 스프링이 미리 생성한 객체에 연결하는 기능)
     @Autowired
@@ -21,14 +24,17 @@ public class ArticleController {
 
     @PostMapping("/article/create")
     public String createArticle(ArticleForm form) {
-        System.out.println(form.toString());
+        log.info(form.toString());
+        //System.out.println(form.toString());
         // DTO 객체 변환 -> Entity
         Article article = form.toEntity();
-        System.out.println(article.toString());
+        log.info(form.toString());
+        //System.out.println(article.toString());
 
         // 레포지토리 에게 엔티티를 DB안에 저장하게 명령
        Article saved = articleRepository.save(article);
-        System.out.println(saved.toString());
+        log.info(form.toString());
+        //System.out.println(saved.toString());
         return "";
     }
 }
--- Spring 시작/firstProject/src/main/java/com/example/firstProject/entity/Article.java
+++ Spring 시작/firstProject/src/main/java/com/example/firstProject/entity/Article.java
@@ -4,6 +4,11 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.ToString;
+
+@AllArgsConstructor
+@ToString
 
 @Entity // DB가 해당 객체를 인식 가능
 public class Article {
@@ -16,19 +21,4 @@ public class Article {
     private String title;
     @Column
     private String content;
-
-    public Article(Long id, String title, String content) {
-        this.id = id;
-        this.title = title;
-        this.content = content;
-    }
-
-    @Override
-    public String toString() {
-        return "Article{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                ", content='" + content + '\'' +
-                '}';
-    }
 }
